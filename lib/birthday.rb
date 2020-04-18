cda8d69873a301d70839a251ec4242f85ee90291
Osha G <ogroetz@gmail.com>
2020-04-18 00:15:11 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,7 +10,7 @@ def happy_birthday(birthday_kids)
   end
 end
 
-require "protect_from_forgery"
+require "pry" 
 def age_appropriate_birthday(kids)
   birthday_kids.each do |kids_name, age|
     binding.pry
