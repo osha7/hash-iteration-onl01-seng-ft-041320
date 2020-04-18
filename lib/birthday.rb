6cfa8fb02c4001398ed16bfc9015958a5487e896
Osha G <ogroetz@gmail.com>
2020-04-18 00:02:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,6 +14,7 @@ def age_appropriate_birthday
   birthday_kids.each do |kids_name, age|
     if age < 13
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
   end
 end
 
